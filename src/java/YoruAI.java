789008f7f9051ed579eb847b6de6d605553c8ff7
LiNKeR <am.usa.to.be@gmail.com>
2018-07-26 20:06:07 +0100

    push

--- src/java/YoruAI.java
+++ src/java/YoruAI.java
@@ -1,6 +1,11 @@
-public class Main {
+package java;
 
+/**
+ * Created by LiNKeR on 7/26/2018.
+ */
+public class YoruAI {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
     }
 }
